78254f8bc64307ab000c1084c88d747ccace28b9
6MA-606 <zyxma.6666@gmail.com>
2023-04-24 11:14:00 +0100

    intial commit

---
+++ bytestream2/Circle.java
@@ -0,0 +1,10 @@
+package bytestream2;
+
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+    
+}
---
+++ bytestream2/JAB2.java
@@ -0,0 +1,39 @@
+package bytestream2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JAB2 {
+
+    public static void main(String[] args) {
+        try {
+            FileOutputStream output = new FileOutputStream("data.dat");
+            String s = "Hello World!";
+
+            byte[] b = s.getBytes();
+            output.write(b);
+            output.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JAB2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JAB2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            FileInputStream input = new FileInputStream("data.dat");
+            int ch;
+            while ((ch = input.read()) != -1) {
+                System.out.println(ch);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JAB2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JAB2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
